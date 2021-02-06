eb6a3f785c5eeee16caaa734bbec00edd197c308
Sean McClure <seanaaron100@gmail.com>
2021-02-06 18:26:54 +0000

    adjusting utilities.js

--- src/scripts/utility.js
+++ src/scripts/utility.js
@@ -1,35 +1,35 @@
 export var utility = {
-    call_once_satisfied : function(props) {
-    if (props['condition'] === true) {
-        if (typeof(props.function) === 'function') {
-            props.function()
+    call_once_satisfied: function(props) {
+        if (props['condition'] === true) {
+            if (typeof(props.function) === 'function') {
+                props.function()
+            } else {
+                props.function()
+            }
         } else {
-            props.function()
+            setTimeout(function() {
+                call_once_satisfied(props)
+            }, 100)
         }
-    } else {
-        setTimeout(function() {
-            call_once_satisfied(props)
-        }, 100)
-    }
-},
-    is_mobile : function() {
+    },
+    is_mobile: function() {
         if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
             return true
         } else {
             return false
         }
     },
-    validate_file_size : function(file, max_allowable_in_mb) {
-    var FileSize = file.files[0].size / 1024 / 1024;
-    if (FileSize > max_allowable_in_mb) {
-        return("not allowed")
-    } else {
-        return("allowed")
-    }
-},
+    validate_file_size: function(file, max_allowable_in_mb) {
+        var FileSize = file.files[0].size / 1024 / 1024;
+        if (FileSize > max_allowable_in_mb) {
+            return ("not allowed")
+        } else {
+            return ("allowed")
+        }
+    },
     character_counter: function(e) {
         var id = e.target.id;
-       // var class_name = document.getElementById(id).className;
+        // var class_name = document.getElementById(id).className;
         var cnt = 280 - Number(e.target.value.length)
         document.getElementById(id).previousElementSibling.innerHTML = cnt;
         if (cnt > 50 && cnt <= 280) {
@@ -41,13 +41,13 @@ export var utility = {
             document.getElementById(id).previousElementSibling.classList.add("rubberBand_it");
         }
     },
-    get_class_instance : function get_class_instance(classname, id) {
-    const elems = document.getElementsByClassName(classname);
-    for (var i = 0; i < elems.length; i++) {
-        if(elems[i].id === id) {
-            var instance = i
+    get_class_instance: function get_class_instance(classname, id) {
+        const elems = document.getElementsByClassName(classname);
+        for (var i = 0; i < elems.length; i++) {
+            if (elems[i].id === id) {
+                var instance = i
+            }
         }
+        return (instance)
     }
-    return(instance)
-}
 }
