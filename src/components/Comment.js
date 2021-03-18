5573cc2ab046dd529641892d1cb991fdaa6522dd
Sean McClure <seanaaron100@gmail.com>
2021-03-18 23:17:42 +0000

    changing textareas in repost and comment to stand alones

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -4,13 +4,29 @@ import Counter from "../components/Counter";
 import Textarea from "../components/Textarea";
 import  WriteIcons from "../components/WriteIcons";
 
+import {utility} from "../scripts/utility.js";
+import {events} from "../scripts/events.js";
+
 function Comment() {
     return(
         <div className="comment_wrapper">
             <div className="hold_comment"></div>
             <div className="hold_textarea_comment">
                 <Counter/>
-                <Textarea/>  
+                <textarea id="write_textarea_comment" className="write_textarea" placeholder="1/n" maxLength="280"
+                    onChange = {(event) => {
+                        utility.character_counter(event);
+                    }}
+                    onClick={(event) => {
+                            events.add_border_on_click(event);
+                            events.disable_delete();
+                    }}
+                    onPaste={(event) => {
+                        setTimeout(function() {
+                            window.current_formula = document.getElementById(event.target.id).value;
+                        }, 100)
+                    }}
+                ></textarea>
             </div>
             <WriteIcons/>
         </div>
--- src/components/Repost.js
+++ src/components/Repost.js
@@ -4,12 +4,28 @@ import Counter from "../components/Counter";
 import Textarea from "../components/Textarea";
 import  WriteIcons from "../components/WriteIcons";
 
+import {utility} from "../scripts/utility.js";
+import {events} from "../scripts/events.js";
+
 function Repost() {
     return(
         <div className="repost_wrapper">
             <div className="hold_textarea_repost">
                 <Counter/>
-                <Textarea/>
+                <textarea id="write_textarea_repost" className="write_textarea" placeholder="1/n" maxLength="280"
+                    onChange = {(event) => {
+                        utility.character_counter(event);
+                    }}
+                    onClick={(event) => {
+                            events.add_border_on_click(event);
+                            events.disable_delete();
+                    }}
+                    onPaste={(event) => {
+                        setTimeout(function() {
+                            window.current_formula = document.getElementById(event.target.id).value;
+                        }, 100)
+                    }}
+                ></textarea>
             </div>  
             <div className="hold_repost"></div>
              <WriteIcons/>
